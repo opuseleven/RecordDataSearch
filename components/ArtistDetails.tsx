1bda384cee99846f045705f2cf0fd9cdede04eb0
Cody Clark <opuseleven@protonmail.com>
2022-06-12 15:03:12 -0500

    Outlined ArtistDetails component

---
+++ components/ArtistDetails.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image';
+import { Artist } from '../types';
+import { useState, useEffect } from 'react';
+
+interface ArtistDetailsProps {
+  artist: Artist
+}
+
+const ArtistDetails: React.FC<ArtistDetailsProps> = ({ artist }) => {
+
+  const [displayedArtist, setDisplayedArtist] = useState(artist);
+
+  useEffect(() => {
+    setDisplayedArtist(artist);
+  }, [artist])
+
+  return (
+    <div>
+      {
+        displayedArtist && (
+          <div>
+            <Image src={displayedArtist.cover_image} />
+            <h3>{displayedArtist.title}</h3>
+          </div>
+        )
+      }
+    </div>
+  )
+}
+export { ArtistDetails };
