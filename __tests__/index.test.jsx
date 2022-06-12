62bb6e4c22e933dc0db4280f8ed470b71065b573
Cody Clark <opuseleven@protonmail.com>
2022-06-12 18:46:29 -0500

    Fixed test for Home page

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -1,10 +1,11 @@
 import Home from '../pages/index';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
 describe('Home page', () => {
 
   it('Page renders', () => {
     render(<Home />);
-    expect(screen.getByRole('heading')).toBeDefined();
+    expect(screen.getByRole('heading')).toHaveTextContent('RecordDataSearch');
   })
 })
