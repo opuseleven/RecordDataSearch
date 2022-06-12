f81ca9f07fdb51ddb65df68b0645f148b4010ac3
Cody Clark <opuseleven@protonmail.com>
2022-06-12 15:04:41 -0500

    Added getAllReleases service

---
+++ services/getAllReleases.ts
@@ -0,0 +1,26 @@
+import { Release, Artist } from '../types';
+import axios from 'axios';
+
+function getAllReleases(artist: Artist): Release[] {
+  
+  let releaseArr: Release[] = [];
+  function setReleaseArr(arr: Release[]) {
+    releaseArr = arr;
+  }
+  let releasesUrl = '';
+  function setReleasesUrl(s: string) {
+    releasesUrl = s;
+  }
+
+
+  axios
+    .get(artist.resource_url)
+    .then((res) => setReleasesUrl(res.data.releases_url));
+  // work with api data
+  axios
+    .get(releasesUrl)
+    .then((res) => setReleaseArr(res.data.releases));
+
+  return releaseArr;
+}
+export { getAllReleases };
