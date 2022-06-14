30c286a25544d70273cef5ed5c1c2f8de2b1300e
Cody Clark <opuseleven@protonmail.com>
2022-06-14 09:19:40 -0500

    Added seperate service to return the releasesUrl string

---
+++ services/getReleasesUrl.ts
@@ -0,0 +1,12 @@
+import { Artist } from '../types';
+import axios from 'axios';
+
+function getReleasesUrl(artist: Artist, setReleasesUrl: React.Dispatch<React.SetStateAction<string>> | any) {
+
+  const url = 'https://api.discogs.com/artists/' + artist.id.toString();
+
+  axios
+    .get(url)
+    .then((res) => setReleasesUrl(res.data.releases_url));
+}
+export { getReleasesUrl };
