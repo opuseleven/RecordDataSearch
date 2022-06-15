56d8dc6967866948d84cf4b3ac4d5710f5c34534
Cody Clark <opuseleven@protonmail.com>
2022-06-15 12:24:06 -0500

    Fixed ResultsContainer component

--- components/ResultsContainer.tsx
+++ components/ResultsContainer.tsx
@@ -1,24 +1,40 @@
 import { useState, useEffect } from 'react';
 import { Release, Artist } from '../types';
 import { ArtistDetails, ReleaseDetails } from '../components';
+import { setAllReleases } from '../services';
 
 interface ResultsContainerProps {
-  releases: Release[],
-  artist: Artist
+  artist: Artist,
+  releasesUrl: string
 }
 
-const ResultsContainer: React.FC<ResultsContainerProps> = ({ releases, artist }) => {
+const ResultsContainer: React.FC<ResultsContainerProps> = ({ artist, releasesUrl }) => {
 
-  const [displayedData, setDisplayedData] = useState({ releases, artist});
+  const [releases, setReleases] = useState<Release[]>();
+  const [displayedArtist, setDisplayedArtist] = useState(artist);
 
   useEffect(() => {
-    setDisplayedData({ releases, artist});
-  }, [releases, artist])
+    setDisplayedArtist(artist);
+    setAllReleases(releasesUrl, setReleases);
+  }, [artist])
 
   return (
     <div>
-      <ArtistDetails artist={displayedData.artist} />
-      <ReleaseDetails releases={displayedData.releases} />
+      <div>
+        {
+          displayedArtist && (
+            <ArtistDetails artist={displayedArtist} />
+          )
+        }
+      </div>
+
+      <div>
+        {
+          releases && (
+            <ReleaseDetails releases={releases} />
+          )
+        }
+      </div>
     </div>
   )
 }
