3102d1b98205850083ff6c425e0dd063f6a4f13c
Cody Clark <opuseleven@protonmail.com>
2022-06-15 12:21:09 -0500

    Fixed setAllReleases service

--- services/setAllReleases.ts
+++ services/setAllReleases.ts
@@ -1,9 +1,9 @@
 import { Release } from '../types';
 import axios from 'axios';
 
-function setAllReleases(url: string, setReleases: React.Dispatch<React.SetStateAction<Release[]>>) {
+function setAllReleases(url: string, setReleases: React.Dispatch<React.SetStateAction<Release[] | undefined>>) {
   axios
     .get(url)
-    .then((res) => setReleases(res.data));
+    .then((res) => setReleases(res.data.releases));
 }
 export { setAllReleases };
