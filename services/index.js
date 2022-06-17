4fa1e3e4413bc67ad514d0b747f08497cbb2a374
Cody Clark <opuseleven@protonmail.com>
2022-06-16 22:06:36 -0500

    Exported getArtist service

--- services/index.js
+++ services/index.js
@@ -2,3 +2,5 @@ export * from './filterByArtists';
 export * from './artistMatch';
 export * from './getReleasesUrl';
 export * from './setAllReleases';
+export * from './filterByHasType';
+export * from './getArtist';
