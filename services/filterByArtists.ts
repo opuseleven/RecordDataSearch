efb6d58a90c25d7cb0f396b4dcecdc6bdbd1b7f4
Cody Clark <opuseleven@protonmail.com>
2022-06-16 22:06:09 -0500

    Fixed filterByArtists function

--- services/filterByArtists.ts
+++ services/filterByArtists.ts
@@ -1,4 +1,11 @@
-function filterByArtists(data: []) {
-  return data.filter(a => a['type'] === 'artist')
+import { Artist, Release } from '../types';
+import { getArtist } from '../services';
+import { filterByHasType } from '.';
+
+function filterByArtists(data: any): Artist[] {
+  const filteredList: (Artist | Release)[] = filterByHasType(data);
+  const artistList = filteredList.filter(a => a['type'] === 'artist');
+  const artistArr: Artist[] = artistList.map(a => getArtist(a));
+  return artistArr;
 }
 export { filterByArtists };
