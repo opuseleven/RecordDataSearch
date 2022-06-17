19d0b7e81a64587205855511c05fbc6c4f7d733a
Cody Clark <opuseleven@protonmail.com>
2022-06-16 22:02:47 -0500

    Made a few parameters optional

--- types/Artist.ts
+++ types/Artist.ts
@@ -1,15 +1,15 @@
 export interface Artist {
   id: number,
   type: string,
-  user_data: {
+  user_data?: {
     in_wantlist: boolean,
     in_collection: boolean
   },
-  master_id: any,
-  master_url: any,
+  master_id?: any,
+  master_url?: any,
   uri: string,
   title: string,
-  thumb: string,
-  cover_image: string,
+  thumb?: string,
+  cover_image?: string,
   resource_url: string
 }
