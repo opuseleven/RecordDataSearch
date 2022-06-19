e411055efa501f64627c9c898cbefd38488f620f
Cody Clark <opuseleven@protonmail.com>
2022-06-19 11:48:56 -0500

    Fixed weird rendering bug

--- components/ReleaseDetails.tsx
+++ components/ReleaseDetails.tsx
@@ -1,18 +1,33 @@
 import { Release  } from '../types';
 import { useState, useEffect } from 'react';
+import { ReleasesNotFoundError } from '../errors';
+import axios from 'axios';
 
 interface ReleaseDetailsProps {
-  releases: Release[] | undefined
+  releasesUrl: string
 }
 
-const ReleaseDetails: React.FC<ReleaseDetailsProps> = ({ releases }) => {
+const ReleaseDetails: React.FC<ReleaseDetailsProps> = ({ releasesUrl }) => {
+
+  const [displayedReleases, setDisplayedReleases] = useState<Release[]>();
+
+  useEffect(() => {
+    try {
+      axios
+        .get(releasesUrl)
+        .then((res) => setDisplayedReleases(res.data.releases))
+    } catch(err) {
+      console.log(err);
+      setDisplayedReleases(ReleasesNotFoundError());
+    }
+  }, [releasesUrl])
 
   return (
     <div>
       <ul>
         {
-          releases && (
-            releases.map((r: Release) => (
+          displayedReleases && (
+            displayedReleases.map((r: Release) => (
               <li key={r.id.toString()}>
                 <h4>{r.title}</h4>
                 <h5>{r.artist}</h5>
--- components/ResultsContainer.tsx
+++ components/ResultsContainer.tsx
@@ -1,22 +1,18 @@
 import { useState, useEffect } from 'react';
-import { Release, Artist } from '../types';
+import {  Artist } from '../types';
 import { ArtistDetails, ReleaseDetails } from '../components';
-import { setAllReleases } from '../services';
 
 interface ResultsContainerProps {
   artist: Artist,
-  releasesUrl: string,
-  releases: Release[],
-  setReleases: React.Dispatch<React.SetStateAction<Release[]>>
+  releasesUrl: string
 }
 
-const ResultsContainer: React.FC<ResultsContainerProps> = ({ artist, releasesUrl, releases, setReleases }) => {
+const ResultsContainer: React.FC<ResultsContainerProps> = ({ artist, releasesUrl }) => {
 
   const [displayedArtist, setDisplayedArtist] = useState(artist);
 
   useEffect(() => {
     setDisplayedArtist(artist);
-    setAllReleases(releasesUrl, setReleases);
   }, [artist])
 
   return (
@@ -25,6 +21,14 @@ const ResultsContainer: React.FC<ResultsContainerProps> = ({ artist, releasesUrl
         {
           displayedArtist && (
             <ArtistDetails artist={displayedArtist} />
+          )
+        }
+      </div>
+
+      <div>
+        {
+          displayedArtist && (
+            <ReleaseDetails releasesUrl={releasesUrl} />
           )
         }
       </div>
