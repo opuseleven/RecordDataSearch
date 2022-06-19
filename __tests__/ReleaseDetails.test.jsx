04bfcc7892586683d4f701bfe7f603a7e6dd3b64
Cody Clark <opuseleven@protonmail.com>
2022-06-19 12:24:39 -0500

    Fixed tests

--- __tests__/ReleaseDetails.test.jsx
+++ __tests__/ReleaseDetails.test.jsx
@@ -1,13 +1,16 @@
 import { ReleaseDetails } from '../components';
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 describe('ReleaseDetails component', () => {
 
-  const testReleases = require('./testdata/releaselist.json');
+  const testUrl = 'https://api.discogs.com/artists/7628210/releases';
 
-  it('Component renders', () => {
-    render(<ReleaseDetails releases={testReleases} />);
+  it('Component renders', async () => {
+    render(<ReleaseDetails releasesUrl={testUrl} />);
+    await act(async () => {
+      await new Promise(r => setTimeout(r, 3000));
+    })
     expect(screen.getAllByRole('heading')[0]).toHaveTextContent('Ghost Stories');
     expect(screen.getAllByRole('heading')[1]).toHaveTextContent('Haunted Like Human');
   })
