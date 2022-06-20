d64e2f930e8af35f9fb179f8d5095a63d694dc3e
Cody Clark <opuseleven@protonmail.com>
2022-06-19 22:58:18 -0500

    Exported new types

--- types/index.js
+++ types/index.js
@@ -1,2 +1,7 @@
 export * from './Release';
 export * from './Artist';
+export * from './DetailedRelease';
+export * from './DiscogObj';
+export * from './Format';
+export * from './Track';
+export * from './ExtraArtist';
