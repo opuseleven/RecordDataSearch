dce7d3efbdd318b681d1e13e219b8df5f69b44c2
Cody Clark <opuseleven@protonmail.com>
2022-06-20 12:33:25 -0500

    Added tests for DisplayRelease component

--- __tests__/DisplayRelease.test.jsx
+++ __tests__/DisplayRelease.test.jsx
@@ -1,5 +1,5 @@
 import { DisplayRelease } from '../components';
-import { render, screen } from '@testing-library/react';
+import { render, screen, act, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 describe('DisplayRelease component', () => {
@@ -12,5 +12,15 @@ describe('DisplayRelease component', () => {
     expect(screen.getAllByRole('heading')[0]).toHaveTextContent('Ghost Stories');
     expect(screen.getAllByRole('heading')[1]).toHaveTextContent('Haunted Like Human');
     expect(screen.getByRole('list')).toBeDefined();
+  })
+
+  it('DetailedRelease info is displayed on "show details" click', async () => {
+    render(<DisplayRelease release={testRelease} />);
+    expect(screen.getAllByRole('heading')[0]).toHaveTextContent('Ghost Stories');
+    await act(async () => {
+      fireEvent.click(screen.getAllByRole('button')[0]);
+      await new Promise((r) => setTimeout(r, 2000));
+    })
+    expect(screen.getAllByRole('heading')[3]).toHaveTextContent('Labels:')
   })
 })
