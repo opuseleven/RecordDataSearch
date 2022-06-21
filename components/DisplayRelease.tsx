caf6c64a20a914cfacfcc497c3d515543b72f2e8
Cody Clark <opuseleven@protonmail.com>
2022-06-21 11:11:14 -0500

    Added displayedRelease state var

--- components/DisplayRelease.tsx
+++ components/DisplayRelease.tsx
@@ -11,10 +11,12 @@ interface DisplayReleaseProps {
 const DisplayRelease: React.FC<DisplayReleaseProps> = ({ release }) => {
 
   const [showMore, setShowMore] = useState<boolean>(false);
+  const [displayedRelease, setDisplayedRelease] = useState(release);
   const [detailedRelease, setDetailedRelease] = useState<DetailedRelease>();
   const token = process.env.TOKEN;
 
   useEffect(() => {
+    setDisplayedRelease(release);
     axios
       .request({
         url: release.resource_url,
@@ -33,14 +35,14 @@ const DisplayRelease: React.FC<DisplayReleaseProps> = ({ release }) => {
 
   return (
     <div className={styles.releasecontainer}>
-      <h4>Release: {release.title}</h4>
-      <h5>Artist(s): {release.artist}</h5>
+      <h4>Release: {displayedRelease.title}</h4>
+      <h5>Artist(s): {displayedRelease.artist}</h5>
       <ul className={styles.detaillist}>
-        <li className={styles.detaillistitem}>Label: {release.label}</li>
-        <li className={styles.detaillistitem}>Release Year: {release.year}</li>
-        <li className={styles.detaillistitem}>Format: {release.format}</li>
+        <li className={styles.detaillistitem}>Label: {displayedRelease.label}</li>
+        <li className={styles.detaillistitem}>Release Year: {displayedRelease.year}</li>
+        <li className={styles.detaillistitem}>Format: {displayedRelease.format}</li>
         <li className={styles.detaillistitem}>
-          <a className={styles.discoglink} href={release.resource_url} target='_blank'>
+          <a className={styles.discoglink} href={displayedRelease.resource_url} target='_blank'>
             View raw data
           </a>
         </li>
