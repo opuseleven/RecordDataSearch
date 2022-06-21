6ed8526331e127bd93a5f2038cc6f2660c5b3eea
Cody Clark <opuseleven@protonmail.com>
2022-06-21 11:08:42 -0500

    Added displayedObj state var

--- components/RenderDiscogObj.tsx
+++ components/RenderDiscogObj.tsx
@@ -1,4 +1,5 @@
 import { DiscogObj } from '../types';
+import { useState, useEffect } from 'react';
 import styles from '../styles/Components.module.css';
 
 interface RenderDiscogObjProps {
@@ -7,21 +8,27 @@ interface RenderDiscogObjProps {
 
 const RenderDiscogObj: React.FC<RenderDiscogObjProps> = ({ obj }) => {
 
+  const [displayedObj, setDisplayedObj] = useState(obj);
+
+  useEffect(() => {
+    setDisplayedObj(obj);
+  }, [obj])
+
   return (
     <div className={styles.objcontainer}>
       <ul>
-        <li><h5>{obj.name}</h5></li>
+        <li><h5>{displayedObj.name}</h5></li>
         <div>
           {
-            obj.entity_type_name && (
-              <li><h5>Type: {obj.entity_type_name}</h5></li>
+            displayedObj.entity_type_name && (
+              <li><h5>Type: {displayedObj.entity_type_name}</h5></li>
             )
           }
         </div>
         <div>
           {
-            obj.role && (
-              <li><h5>Role: {obj.role}</h5></li>
+            displayedObj.role && (
+              <li><h5>Role: {displayedObj.role}</h5></li>
             )
           }
         </div>
