cdf8bfadb7d8407ae401de4ff27b23b323b382be
Cody Clark <opuseleven@protonmail.com>
2022-06-20 19:37:23 -0500

    Fixed axios requests

--- services/getReleasesUrl.ts
+++ services/getReleasesUrl.ts
@@ -4,9 +4,21 @@ import axios from 'axios';
 function getReleasesUrl(artist: Artist, setReleasesUrl: React.Dispatch<React.SetStateAction<string>>) {
 
   const url = 'https://api.discogs.com/artists/' + artist.id.toString();
+  const token = process.env.TOKEN;
 
   axios
-    .get(url)
-    .then((res) => setReleasesUrl(res.data.releases_url));
+    .request({
+      url: url,
+      method: 'get',
+      responseType: 'json',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Discogs token=' + token
+      }
+    })
+    .then((res) => setReleasesUrl(res.data.releases_url))
+    .catch((err) => {
+      console.log(err);
+    });
 }
 export { getReleasesUrl };
--- services/setAllReleases.ts
+++ services/setAllReleases.ts
@@ -2,8 +2,19 @@ import { Release } from '../types';
 import axios from 'axios';
 
 function setAllReleases(url: string, setReleases: React.Dispatch<React.SetStateAction<Release[] | undefined>>) {
+
+  const token = process.env.TOKEN;
+
   axios
-    .get(url)
+    .request({
+      url: url,
+      method: 'get',
+      responseType: 'json',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Discogs token=' + token
+      }
+    })
     .then((res) => setReleases(res.data.releases));
 }
 export { setAllReleases };
