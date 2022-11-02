2cb28cbf46b6897428a17c93c6895367ab0529e5
Cody Clark <opuseleven@protonmail.com>
2022-11-02 11:04:00 -0500

    Added additional test for ResultsPageNav component

--- __tests__/ResultsPageNav.test.jsx
+++ __tests__/ResultsPageNav.test.jsx
@@ -34,6 +34,28 @@ describe('ResultsPageNav component', () => {
       await new Promise(r => setTimeout(r, 2000));
     })
     expect(page).toBe(2);
+  })
 
+  it('UI functions to update page state back a page', async () => {
+    const testPagination = {
+      page: 2,
+      pages: 4,
+      per_page: 5,
+      item: 19,
+      urls: {
+        last: '',
+        next: ''
+      }
+    }
+    render(<ResultsPageNav page={page} setPage={setPage} pagination={testPagination} />);
+    expect(screen.getByRole('heading')).toHaveTextContent('2');
+    const buttons = screen.getAllByRole('button');
+    expect(buttons[0]).toHaveTextContent('Back page');
+    expect(buttons[1]).toHaveTextContent('Next page');
+    await act(async () => {
+      fireEvent.click(screen.getByText('Back page'));
+      await new Promise(r => setTimeout(r, 2000));
+    })
+    expect(page).toBe(1);
   })
 })
